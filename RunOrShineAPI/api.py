1397424c3811720608b813c5e3ab5ecffd6c49b0
Ryan DeBose-Boyd <ryan96db@gmail.com>
2020-02-29 14:39:41 -0600

    Connect Database to API

--- RunOrShineAPI/api.py
+++ RunOrShineAPI/api.py
@@ -1,27 +1,20 @@
 import flask
 from flask import request, jsonify
+import sqlite3
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-# Items
-
-items = [
-    {'id': 1,
-     'name': "Tank Top",
-     'description': "XXX"},
-    {'id': 2,
-     'name': "Shorts",
-     'description': "XXX"},
-    {'id': 3,
-     'name': "Short-Sleeve T-Shirt",
-     'description': "XXX"}
-    
 
 
 
+def dict_factory(cursor, row):
+    d = {}
+    for indx, colum in enumerate(cursor.description):
+        d[colum[0]] = row[indx]
+    return d
 
 
-]
+
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>RunOrShineAPI</h1><p>Prototype</p>"
@@ -29,33 +22,48 @@ def home():
 # A route to return running gear.
 
 @app.route('/api/v1/resources/items/all', methods=['GET'])
-def api_all():
-    return jsonify(items)
 
-#Adds ability to filter items
+def api_all():
+    conect = sqlite3.connect('items.db')
+    conect.row_factory = dict_factory
+    curs = conect.cursor()
+    all_items = curs.execute('SELECT * FROM items;').fetchall()
+
+    return jsonify(all_items)
+
+
+@app.errorhandler(404)
+def page_not_found(er):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+
+#Adds ability to filter items by id
 
 @app.route('/api/v1/resources/items', methods=['GET'])
+def api_filter():
+    query_params = request.args
 
-def api_id():
-    # Check if ID was provided as part of URL.
-    # Assigns ID to variable if ID is provided.
-    # Displays erros in browser if no ID is provided.
+    id = query_params.get('id')
 
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return "Error: No ID field provided. Please specify ID."
+    query = "SELECT * FROM items WHERE"
+    to_filter = []
 
-    # Empty list for item results
-    results = []
+    if id:
+        query += ' id=? AND'
+        to_filter.append(id)
+    if not (id):
+        return page_not_found(404)
 
-    # Loop through data and match results that fit the ID that was requested.
+    query = query[:-4] + ';'
 
-    for item in items:
-        if item['id'] == id:
-            results.append(item)
+    conect = sqlite3.connect('items.db')
+    conect.row_factory = dict_factory
+    curs = conect.cursor()
+
+    results = curs.execute(query, to_filter).fetchall()
 
     return jsonify(results)
+
 
 
 app.run()
